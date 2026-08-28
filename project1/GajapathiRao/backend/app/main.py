e658d0bd255e916f2e99308270e75cb779fc07da
GajapathiRaoAmpera <gajapathi.mugunda@amperatech.ai>
2026-08-28 11:31:29 +0530

    Added Weatherly Hour Endppoint

--- project1/GajapathiRao/backend/app/main.py
+++ project1/GajapathiRao/backend/app/main.py
@@ -1,4 +1,5 @@
 import fastapi as fastapi
+from app.database.connection import create_connection
 
 app = fastapi.FastAPI(
     title="Weather ETL API",
@@ -12,3 +13,45 @@ def read_root():
      return {
         "message": "Weather ETL API is running"
     }
+     
+     
+@app.get("/weather/hourly")
+def get_hourly_weather():
+
+    connection = create_connection()
+
+    try:
+        with connection.cursor() as cursor:
+
+            cursor.execute("""
+                SELECT
+                    city,
+                    state,
+                    country,
+                    observed_at,
+                    temperature,
+                    humidity,
+                    wind_speed
+                FROM vw_weather_hourly
+                ORDER BY observed_at;
+            """)
+
+            rows = cursor.fetchall()
+
+            columns = [
+                "city",
+                "state",
+                "country",
+                "observed_at",
+                "temperature",
+                "humidity",
+                "wind_speed",
+            ]
+
+            return [
+                dict(zip(columns, row))
+                for row in rows
+            ]
+
+    finally:
+        connection.close()
