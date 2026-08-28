7870a0d56fb8aad37eacc39592e1da6d93344bec
GajapathiRaoAmpera <gajapathi.mugunda@amperatech.ai>
2026-08-28 17:06:03 +0530

    Added basic structure

---
+++ project1/GajapathiRao/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./styles/main.scss";
+
+function App() {
+    return (
+        <div className="app">
+            <h1>Weather ETL Platform</h1>
+            <p>Weather monitoring and ETL dashboard</p>
+        </div>
+    );
+}
+
+export default App;
