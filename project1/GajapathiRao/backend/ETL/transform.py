d073f902d7f81714c67c926917e08f0cad2bc20f
GajapathiRaoAmpera <gajapathi.mugunda@amperatech.ai>
2026-08-28 12:56:20 +0530

    Modified transform in ETL for location specific

--- project1/GajapathiRao/backend/ETL/transform.py
+++ project1/GajapathiRao/backend/ETL/transform.py
@@ -15,17 +15,52 @@ def transform_hourly_data(data: dict) -> pd.DataFrame:
 
     return df
 
+def transform_all_locations(results):
+
+    all_data = []
+
+    for result in results:
+
+        location = result["location"]
+        weather = result["weather"]
+
+        hourly = weather["hourly"]
+
+        df = pd.DataFrame({
+            "observed_at": pd.to_datetime(hourly["time"]),
+            "temperature": hourly["temperature_2m"],
+            "humidity": hourly["relative_humidity_2m"],
+            "wind_speed": hourly["wind_speed_10m"],
+        })
+
+        df["location_id"] = location["location_id"]
+
+        all_data.append(df)
+
+    return pd.concat(all_data, ignore_index=True)
+
 if __name__ == "__main__":
-    from extract import fetch_weather
+    from extract import fetch_weather , fetch_weather_for_all_locations
 
-    latitude = 13.110721
-    longitude = 80.2459
+    # latitude = 13.110721
+    # longitude = 80.2459
 
-    data = fetch_weather(latitude, longitude)
+    # data = fetch_weather(latitude, longitude)
 
-    df = transform_hourly_data(data)
+    # df = transform_hourly_data(data)
+
+    # print(df.head())
+    # print("\nShape:", df.shape)
+    # print("\nData types:")
+    # print(df.dtypes)
+    
+    
+
+    results = fetch_weather_for_all_locations()
+
+    df = transform_all_locations(results)
 
     print(df.head())
     print("\nShape:", df.shape)
-    print("\nData types:")
-    print(df.dtypes)
+    print("\nRecords by location:")
+    print(df.groupby("location_id").size())
