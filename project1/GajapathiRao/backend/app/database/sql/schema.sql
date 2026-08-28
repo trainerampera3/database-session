e121e68775c3ca57b6d01e945d19220f52613788
GajapathiRaoAmpera <gajapathi.mugunda@amperatech.ai>
2026-08-28 14:03:21 +0530

    Added table and updated logs

--- project1/GajapathiRao/backend/app/database/sql/schema.sql
+++ project1/GajapathiRao/backend/app/database/sql/schema.sql
@@ -40,3 +40,19 @@ CREATE TABLE weather_hourly (
 
     UNIQUE (location_id, observed_at)
 );
+
+CREATE TABLE weather_historical (
+    historical_id BIGSERIAL PRIMARY KEY,
+    location_id INT NOT NULL,
+    observed_at TIMESTAMP NOT NULL,
+    temperature NUMERIC(5,2),
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_weather_historical_location
+        FOREIGN KEY (location_id)
+        REFERENCES location(location_id),
+
+    CONSTRAINT uq_weather_historical_location_time
+        UNIQUE (location_id, observed_at)
+);
